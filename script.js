795d0d1e60e6438864c2a1778800a6b54c3a5601
Tulio <tuliomarcos123456@gmail.com>
2022-07-20 20:23:02 -0300

    Exercicio 2 / Modulo 1

---
+++ script.js
@@ -0,0 +1,14 @@
+/* 
+o Professor organiza Cada linha com 6 alunos.
+ Cada Linha contem 4 alunos que gostam da mesma materia.
+
+Na Questão informa :  percebeu que um aluno que gosta de uma das disciplinas 
+					  havia trocado de lugar com alguém que prefere a outra.
+
+
+
+o aluno de numero 5 da sequencia esquerda para direita na Linha 5 mudou de caderia com o numero 4
+da mesma linha ;
+o numero 5 mudou para (P).
+e o numero 4 Continuou gostando de (P).
+*/
