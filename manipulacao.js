5055be780bf06a8517aed9e10b26b234012c7f99
Túlio Marcos <80016867+Tulio-Marcos@users.noreply.github.com>
2022-08-18 09:33:33 -0300

    Add files via upload

---
+++ manipulacao.js
@@ -0,0 +1,19 @@
+var texto = prompt("Digite uma palavra")
+function maiuscular (){
+  var uper= texto.toUpperCase();
+   alert(uper);
+
+}
+
+maiuscular();
+function minuscular(){
+  var lower= texto.toLowerCase()
+  alert(lower)
+}
+minuscular();
+
+function tamanho(){
+  var tamanho = texto.length
+  alert(tamanho)
+}
+tamanho();
